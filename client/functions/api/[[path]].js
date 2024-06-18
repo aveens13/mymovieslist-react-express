cbf10a672e2370a9b1e6440027545c86b98b0155
Sudhan Bhattarai <bhattaraiavinav400@gmail.com>
2024-06-18 15:07:15 +0545

    Added api to path

--- client/functions/api/[[path]].js
+++ client/functions/api/[[path]].js
@@ -2,7 +2,7 @@ export async function onRequest({ request }) {
   const url = new URL(request.url);
   url.hostname = "movieridge.eastus.cloudapp.azure.com";
   url.protocol = "http:";
-  url.pathname = url.pathname.slice(url.pathname.indexOf("/", 1));
+  url.pathname = "api/" + url.pathname.slice(url.pathname.indexOf("/", 1));
   url.port = "";
   return fetch(url.toString(), request);
 }
