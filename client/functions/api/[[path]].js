b916700530f816bb5082a77fef38f49134ad8401
Sudhan Bhattarai <bhattaraiavinav400@gmail.com>
2024-07-10 23:24:35 +0545

    Secured Nginx Server with Certbot

--- client/functions/api/[[path]].js
+++ client/functions/api/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
   const url = new URL(request.url);
   url.hostname = "movieridge.eastus.cloudapp.azure.com";
-  url.protocol = "http:";
+  url.protocol = "https:";
   url.pathname = "api" + url.pathname.slice(url.pathname.indexOf("/", 1));
   url.port = "";
   return fetch(url.toString(), request);
