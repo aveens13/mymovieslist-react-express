00459dcc8a5f941ce7eefa65c174b17cb3ebe437
Sudhan Bhattarai <bhattaraiavinav400@gmail.com>
2024-12-13 09:40:48 +0545

    fix: hashing password bug fix

--- Router/authRoutes.js
+++ Router/authRoutes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const handlers = require("../lib/authenticationLogic");
+const { hashPassword } = require("../lib/middleware");
 const router = Router();
 
 router.get("/api/verifyToken", handlers.checktoken);
 router.post("/api/signup", handlers.signup_post);
 router.post("/api/login", handlers.login_post);
-router.post("/api/validateOTP", handlers.validateOTP);
+router.post("/api/validateOTP", hashPassword, handlers.validateOTP);
 router.get("/api/logout", handlers.logout);
 
 module.exports = router;
